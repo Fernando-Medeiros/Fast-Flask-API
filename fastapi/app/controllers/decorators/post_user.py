78d71a0de863b5128e5617db294b5e4ce160a890
Fernando-Medeiros <fernandobasilides@gmail.com>
2022-12-03 22:22:03 -0300

    feat-fastapi: Add a rota auth/login retornando 'access_token':TokenJwt

--- fastapi/app/controllers/decorators/post_user.py
+++ fastapi/app/controllers/decorators/post_user.py
@@ -1,17 +1,43 @@
 from functools import wraps
 
 import ormar
+from app.utils.token_jwt import CreateTokenJwt
+from werkzeug.security import check_password_hash
+
+from fastapi import Form, HTTPException
 
 
-def post(model : ormar.Model):
+def post(model: ormar.Model):
     def inner (func):
 
         @wraps(func)
-        async def wrap(request_model: ormar.Model):
+        async def wrapper(request_model: ormar.Model):
             attr = request_model.dict(exclude_unset=True)
             user = model(**attr)
             return await user.save()
 
-        return wrap
+        return wrapper
+    return inner
+
+
+def post_login(model: ormar.Model):
+    def inner (func):
+
+        @wraps(func)
+        async def wrapper(email: str = Form(...), password: str = Form(...)):
+            
+            user = await model.objects.get_or_none(email=email)
+            
+            if not user or not check_password_hash(user.password, password):
+                raise HTTPException(
+                    status_code=403,
+                    detail='User not found'
+                )
+            return {
+                'access_token': CreateTokenJwt().create_token(id=user.id),
+                'token_type': 'bearer'
+            }
+           
+        return wrapper
     return inner
 
--- fastapi/app/controllers/user_controller.py
+++ fastapi/app/controllers/user_controller.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Form
 
 from ..models.user import UserModel, UserRequest, UserResponse
 from .decorators import post_user
@@ -8,4 +8,10 @@ router = APIRouter()
 @router.post('/', response_model=UserResponse)
 @post_user.post(UserModel)
 async def create(request_model: UserRequest):
+    pass
+
+
+@router.post('/login')
+@post_user.post_login(UserModel)
+async def login(email: str = Form(...), password: str = Form(...)):
     pass
