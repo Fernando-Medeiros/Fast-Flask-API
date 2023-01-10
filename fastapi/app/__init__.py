da689a6b0da6f936c5c63b3005eb6ba71a2e4b13
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-01-09 21:10:32 -0300

    Add: CORSMiddleware, e ALLOWED_HOSTS no .env.example

--- fastapi/app/__init__.py
+++ fastapi/app/__init__.py
@@ -1,12 +1,24 @@
+import os
+
 from setup import conf_database
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routes import router
 
 
 def app() -> FastAPI:
     app = FastAPI()
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=list(str(os.getenv('ALLOWED_HOSTS')).split(',')),
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+        )
+    
     app.include_router(router, prefix='')
     conf_database()
     return app
