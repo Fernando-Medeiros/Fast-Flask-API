fd48001387219c1cfb0368a5ba4f55306e87afb7
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-01-11 21:49:56 -0300

    refactor: atualização dos modelos de post

--- app/models/post/post.py
+++ app/models/post/post.py
@@ -2,10 +2,10 @@ import re
 from datetime import datetime
 
 import ormar
-from pydantic import validator
-from setup import database, metadata
-
 from fastapi import HTTPException, status
+from pydantic import validator
+
+from setup import database, metadata
 
 from ..user import UserModel
 
@@ -14,26 +14,22 @@ class PostModel(ormar.Model):
     class Meta:
         database = database
         metadata = metadata
-        tablename = 'posts'
+        tablename = "posts"
 
     id = ormar.Integer(primary_key=True, autoincrement=True)
     author = ormar.ForeignKey(
         UserModel,
-        related_name='posts',
-        onupdate=ormar.ReferentialAction('CASCADE'),
-        ondelete=ormar.ReferentialAction('CASCADE'),
-        )
+        related_name="posts",
+        onupdate=ormar.ReferentialAction("CASCADE"),
+        ondelete=ormar.ReferentialAction("CASCADE"),
+    )
     date = ormar.Date(default=datetime.today().date())
     time = ormar.Time(default=datetime.today().time())
-    content = ormar.Text(null=False)
+    content = ormar.Text(nullable=False)
 
-    
-    @validator('content')
+    @validator("content")
     def _content(cls, value):
-        
-        if not re.compile(r'^([A-Za-z0-9]).{3,}$').match(value):
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail='Invalid content!'
-            )
-        return value
+
+        if re.compile(r"^([A-Za-z0-9]).{3,}$").match(value):
+            return value
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, detail="Invalid content")
--- app/models/post/request.py
+++ app/models/post/request.py
@@ -1,33 +1,18 @@
 import re
 
-from pydantic import BaseModel, validator
-
 from fastapi import HTTPException, status
+from pydantic import BaseModel, validator
 
 
 class PostRequest(BaseModel):
     content: str
 
-    @validator('content')
+    @validator("content")
     def _content(cls, value):
-        
-        if not re.compile(r'^([A-Za-z0-9]).{3,}$').match(value):
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail='Invalid content!'
-            )
-        return value
+        if re.compile(r"^([A-Za-z0-9]).{3,}$").match(value):
+            return value
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, detail="Invalid content")
 
 
-class PostRequestPatch(BaseModel):
-    content: str
-
-    @validator('content')
-    def _content(cls, value):
-        
-        if not re.compile(r'^([A-Za-z0-9]).{3,}$').match(value):
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail='Invalid content!'
-            )
-        return value
+class PostRequestPatch(PostRequest):
+    ...
