c2c083547db60d13eedede22d4a2e02740e68e8a
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-01-11 21:06:20 -0300

    feat-fastapi: atualização das rotas /users

--- app/controllers/user.py
+++ app/controllers/user.py
@@ -1,59 +1,63 @@
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 
-from ..models.user import (UserModel, UserRequest, UserRequestPatch,
-                           UserRequestUpdatePassword, UserResponse,
-                           UserResponseAccountData)
+from ..models.user import (
+    UserModel,
+    UserRequest,
+    UserRequestPatch,
+    UserRequestUpdatePassword,
+    UserResponse,
+    UserResponseAccountData,
+)
 from ..utils.login_required import login_required
 from .decorators import user_controller
 
 router = APIRouter()
 
 
-@router.get('/', response_model=List[UserResponse])
+@router.get("", response_model=List[UserResponse])
 @user_controller.get_all(UserModel)
 async def get_list_with_all_users():
     ...
 
 
-@router.get('/{username}', response_model=UserResponse)
+@router.get("/{username}", response_model=UserResponse)
 @user_controller.get_by_username(UserModel)
-async def get_by_username(username: str):
+async def get_user_by_username(username: str):
     ...
 
 
-@router.post('/', response_model=UserResponse)
+@router.post("", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 @user_controller.create_account(UserModel)
 async def create_new_account(request_model: UserRequest):
     ...
 
 
-@router.patch('/', response_model=UserResponse)
+@router.patch("", response_model=UserResponse, status_code=status.HTTP_200_OK)
 @user_controller.update_user(UserModel)
 async def update_user(
-    request_model: UserRequestPatch,
-    current_user: UserModel = Depends(login_required)):
+    request_model: UserRequestPatch, current_user: UserModel = Depends(login_required)
+):
     ...
 
 
-@router.patch('/update-password/')
+@router.patch("/update_password")
 @user_controller.update_password(UserModel)
 async def update_password(
     request_model: UserRequestUpdatePassword,
-    current_user: UserModel = Depends(login_required)):
+    current_user: UserModel = Depends(login_required),
+):
     ...
 
 
-@router.delete('/')
+@router.delete("")
 @user_controller.delete(UserModel)
-async def delete_account(
-    current_user: UserModel = Depends(login_required)):
+async def delete_account(current_user: UserModel = Depends(login_required)):
     ...
 
 
-@router.get('/account-data/', response_model=UserResponseAccountData)
+@router.get("/account_data/", response_model=UserResponseAccountData)
 @user_controller.get_account_data(UserModel)
-async def get_account_data(
-    current_user: UserModel = Depends(login_required)):
+async def get_account_data(current_user: UserModel = Depends(login_required)):
     ...
