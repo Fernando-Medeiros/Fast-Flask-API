dd14fd5e1d04fa4b628043274b5cf361d39f70f8
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-01-12 22:31:53 -0300

    rafactor: atualização na rota /posts

--- app/controllers/decorators/post_controller.py
+++ app/controllers/decorators/post_controller.py
@@ -1,87 +1,52 @@
-from functools import wraps
+from datetime import datetime
 
-import ormar
 from fastapi import HTTPException, status
-from pydantic import BaseModel
+
+from ...models.post import PostModel
+
+model = PostModel
 
 
-def get_all(model: ormar.Model):
-    def inner(func):
-        @wraps(func)
-        async def wrapper():
-            return await model.objects.all()
-
-        return wrapper
-
-    return inner
+async def get_all():
+    return await model.objects.all()
 
 
-def get_by_id(model: ormar.Model):
-    def inner(func):
-        @wraps(func)
-        async def wrapper(id: int):
-            post = await model.objects.get_or_none(id=id)
-            if post:
-                return post
+async def get_by_id(id):
+    post = await model.objects.get_or_none(id=id)
+    if post:
+        return post
 
-            raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Id not found")
-
-        return wrapper
-
-    return inner
+    raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Id not found")
 
 
-def get_by_username(model: ormar.Model):
-    def inner(func):
-        @wraps(func)
-        async def wrapper(username: str):
-            entity = await model.objects.get_or_none(username=username)
-            if entity:
-                posts = await entity.posts.all()
-                return posts
+async def get_by_username(model, username):
+    entity = await model.objects.get_or_none(username=username)
+    if entity:
+        posts = await entity.posts.all()
+        return posts
 
-            raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Username not found")
-
-        return wrapper
-
-    return inner
+    raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Username not found")
 
 
-def patch(model: ormar.Model):
-    def inner(func):
-        @wraps(func)
-        async def wrapper(id: int, request_model: BaseModel, current_user):
-            post = await current_user.posts.get_or_none(id=id)
-            if post:
-                updates = request_model.dict(exclude_unset=True)
-                return await post.update(**updates)
-
-        return wrapper
-
-    return inner
+async def update(id, request_model, current_user):
+    post = await current_user.posts.get_or_none(id=id)
+    if post:
+        updates = request_model.dict(exclude_unset=True)
+        return await post.update(**updates)
 
 
-def post(model: ormar.Model):
-    def inner(func):
-        @wraps(func)
-        async def wrapper(request_model: BaseModel, current_user):
-            if current_user.id:
-                attr = request_model.dict(exclude_unset=True)
-                post = model(author=current_user, **attr)
-                return await post.save()
-
-        return wrapper
-
-    return inner
+async def create(request_model, current_user):
+    if current_user.id:
+        data = request_model.dict(exclude_unset=True)
+        post = model(
+            author=current_user,
+            date=datetime.today().date(),
+            time=datetime.today().time(),
+            **data
+        )
+        return await post.save()
 
 
-def delete(model: ormar.Model):
-    def inner(func):
-        @wraps(func)
-        async def wrapper(id: int, current_user):
-            if await model.objects.delete(id=id, author=current_user.id):
-                return status.HTTP_200_OK, "Post deleted"
-
-        return wrapper
-
-    return inner
+async def delete(id, current_user):
+    if await model.objects.delete(id=id, author=current_user.id):
+        return status.HTTP_200_OK, "Post deleted"
--- app/controllers/post.py
+++ app/controllers/post.py
@@ -2,51 +2,50 @@ from typing import List
 
 from fastapi import APIRouter, Depends, status
 
-from ..models.post import PostModel, PostRequest, PostRequestPatch, PostResponse
+from ..models.post import PostRequest, PostRequestPatch, PostResponse
 from ..models.user import UserModel
 from ..utils.login_required import login_required
 from .decorators import post_controller
 
 router = APIRouter()
+routerAuth = APIRouter()
 
 
 @router.get("", response_model=List[PostResponse])
-@post_controller.get_all(PostModel)
 async def list_posts():
-    ...
+
+    return await post_controller.get_all()
 
 
 @router.get("/{id}", response_model=PostResponse)
-@post_controller.get_by_id(PostModel)
 async def get_post_by_id(id: int):
-    ...
+
+    return await post_controller.get_by_id(id)
 
 
 @router.get("/user/{username}", response_model=List[PostResponse])
-@post_controller.get_by_username(UserModel)
 async def get_posts_by_username(username: str):
-    ...
+
+    return await post_controller.get_by_username(UserModel, username)
 
 
-@router.post("", response_model=PostResponse, status_code=status.HTTP_201_CREATED)
-@post_controller.post(PostModel)
+@routerAuth.post("", response_model=PostResponse, status_code=status.HTTP_201_CREATED)
 async def create_post(
     request_model: PostRequest, current_user: UserModel = Depends(login_required)
 ):
-    ...
+    return await post_controller.create(request_model, current_user)
 
 
-@router.patch("/{id}", response_model=PostResponse)
-@post_controller.patch(PostModel)
+@routerAuth.patch("/{id}", response_model=PostResponse)
 async def edit_post(
     id: int,
     request_model: PostRequestPatch,
     current_user: UserModel = Depends(login_required),
 ):
-    ...
+    return await post_controller.update(id, request_model, current_user)
 
 
-@router.delete("/{id}")
-@post_controller.delete(PostModel)
+@routerAuth.delete("/{id}")
 async def delete_post(id: int, current_user: UserModel = Depends(login_required)):
-    ...
+
+    return await post_controller.delete(id, current_user)
