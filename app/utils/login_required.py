369fbb4488db2d6e31c56f945b8574bfb32c9649
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-01-14 05:02:04 -0300

    feat-fastapi: Add httpexception e atualização dos metodos de validação de usuário e credenciais

--- app/utils/login_required.py
+++ app/utils/login_required.py
@@ -2,10 +2,13 @@ import ormar
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError
+from werkzeug.security import check_password_hash
 
 from ..models.token import TokenData
 from ..models.user import UserModel
 from .token_jwt import DecodeTokenJwt
+
+model: ormar.Model = UserModel
 
 
 class AuthBearer:
@@ -13,32 +16,44 @@ class AuthBearer:
     auth_scheme = OAuth2PasswordBearer(tokenUrl=url)
 
 
-class ValidateCredentials:
-    def validate(self, token: str) -> TokenData:
-        try:
-            payload: dict = DecodeTokenJwt().decode(token)
-            return TokenData(**payload)
+def validate_credentials(token: str) -> TokenData:
+    try:
 
-        except JWTError:
-            raise HTTPException(
-                status.HTTP_401_UNAUTHORIZED,
-                detail="Could not validate credentials",
-                headers={"WWW-Authenticate": "Bearer"},
-            )
+        payload: dict = DecodeTokenJwt.decode(token)
+        return TokenData(**payload)
 
-
-class ValidateUser:
-    model: ormar.Model = UserModel
-
-    async def get_or_none(self, payload) -> UserModel | None:
-        return await self.model.objects.get_or_none(id=payload.id)
+    except JWTError:
+        raise HTTPException(
+            status.HTTP_401_UNAUTHORIZED,
+            detail="could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
 
 
-async def request_token(
-    token: str = Depends(AuthBearer.auth_scheme),
-) -> UserModel | None:
-    payload = ValidateCredentials().validate(token)
-    user = await ValidateUser().get_or_none(payload)
+async def validate_user(payload: TokenData) -> UserModel:
+    user = await model.objects.get_or_none(id=payload.id, username=payload.username)
+    if user:
+        return user
+    raise HTTPException(status.HTTP_404_NOT_FOUND, detail="user not found")
+
+
+async def authenticate_user(form_data) -> UserModel:
+    try:
+        user = await model.objects.get(email=form_data.username)
+        if not check_password_hash(user.password, form_data.password):
+            raise
+        return user
+    except:
+        raise HTTPException(
+            status.HTTP_401_UNAUTHORIZED,
+            detail="user not found or invalid password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+
+async def request_token(token: str = Depends(AuthBearer.auth_scheme)) -> UserModel:
+    payload = validate_credentials(token)
+    user = await validate_user(payload)
     return user
 
 
