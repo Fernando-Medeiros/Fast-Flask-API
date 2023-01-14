760de36af5b6600cd5c2dbfacb9ca97aa457ca0c
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-01-14 05:12:06 -0300

    test: Add atualização dos testes de unidade do tokenJwt

--- tests/unity/test_token_jwt.py
+++ tests/unity/test_token_jwt.py
@@ -1,38 +1,44 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import pytest
+from jose.exceptions import ExpiredSignatureError
 
-from app.utils.token_jwt import CreateTokenJwt, DecodeTokenJwt
+from app.utils.token_jwt import DecodeTokenJwt, TokenJwt
 
-from ..utils.token import TestToken
-
-token = CreateTokenJwt()
-utils = TestToken()
+from ..utils.token import CaseToken
 
 
 @pytest.mark.tokenJwt
 class TestTokenJwt:
-    context = utils.create_context()
+    context = CaseToken().create_context()
 
-    def test_token_expire(self):
-        expire = token.expire()
+    def test_expire_minutes(self):
+        expire = TokenJwt.expire(15)
 
-        assert type(expire) == datetime
-        assert expire.hour != datetime.utcnow().hour
+        assert issubclass(type(expire), datetime)
+        assert expire.minute != datetime.utcnow().minute
 
-    def test_token_body(self):
-        body = token.body(**self.context)
+    def test_create_access_token(self):
+        token = TokenJwt.create_access_token(**self.context)
 
-        assert [body[key] for key in self.context]
+        assert issubclass(type(token), str)
+        assert len(token) > 150
 
-    def test_token_create(self):
-        create_token = token.create_token(**self.context)
+    def test_create_refresh_token(self):
+        token = TokenJwt.create_refresh_token()
 
-        assert type(create_token) == str
-        assert len(create_token) > 200
+        assert issubclass(type(token), str)
+        assert len(token) > 150
 
-    def test_token_decode(self):
-        create_token = token.create_token(**self.context)
-        t_decode = DecodeTokenJwt().decode(create_token)
+    def test_decode(self):
+        token = TokenJwt.create_access_token(**self.context)
+        t_decode = DecodeTokenJwt.decode(token)
 
         assert [t_decode[key] for key in self.context]
+
+    def test_expired(self):
+        expire = datetime.utcnow() - timedelta(minutes=5)
+        token = TokenJwt.create_access_token(exp=expire, **self.context)
+
+        with pytest.raises(ExpiredSignatureError):
+            DecodeTokenJwt.decode(token)
